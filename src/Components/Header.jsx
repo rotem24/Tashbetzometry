2f4555da27f2b9aed220215592bddb0c2f92521a
bgroup11 <64021263+bgroup11@users.noreply.github.com>
2020-07-04 23:08:11 +0300

    settings+header(color)

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -24,6 +24,7 @@ import EditOutlinedIcon from '@material-ui/icons/EditOutlined';
 import Badge from '@material-ui/core/Badge';
 //Context Api
 import { UserDetailsContext } from '../Contexts/UserDetailsContext';
+import { logDOM } from '@testing-library/react';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -74,11 +75,13 @@ function Header(props) {
 
   const classes = useStyles();
   const history = useHistory();
-
   const [open, setOpen] = React.useState(false);
   const theme = useTheme();
   const [color, setColor] = useState(user.Theme);
+
+
   const [badgeContent, setBadgeContent] = useState(0);
+
 
   var local = false;
   var apiUrl = 'http://proj.ruppin.ac.il/bgroup11/prod/api/'
@@ -88,24 +91,28 @@ function Header(props) {
 
   useEffect(() => {
     GetSharedCross()
-}, []);
+  });
 
-const GetSharedCross = async () => {
+  const GetSharedCross = async () => {
+    var c = localStorage.getItem("color");
+    if (color !==c ) {
+      setColor(c);
+    }
     try {
-        const res = await fetch(apiUrl + 'SharedCross/' + user.Mail + '/', {
-          method: 'GET',
-          headers: new Headers({
-            'Content-Type': 'application/json; charset=UTF-8',
-          })
+      const res = await fetch(apiUrl + 'SharedCross/' + user.Mail + '/', {
+        method: 'GET',
+        headers: new Headers({
+          'Content-Type': 'application/json; charset=UTF-8',
         })
-        let result = await res.json();
-        console.log("SharedCross:",result);
-        setBadgeContent(result.length);            
-        
-      } catch (error) {
-        console.log("ErrorSharedCross", error);
-      }
-}
+      })
+      let result = await res.json();
+      console.log("SharedCross:", result);
+      setBadgeContent(result.length);
+
+    } catch (error) {
+      console.log("ErrorSharedCross", error);
+    }
+  }
 
   //עדכון ניקוד למשתמש בDB
   const PutScore = async () => {
