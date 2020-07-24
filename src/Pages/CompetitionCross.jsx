2d22250aa51b32bbf96cbabeb69f9436036ba326
bgroup11 <64021263+bgroup11@users.noreply.github.com>
2020-07-24 23:01:47 +0300

    Competition

--- src/Pages/CompetitionCross.jsx
+++ src/Pages/CompetitionCross.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import  React ,{useEffect } from 'react';
 import { makeStyles, Container } from '@material-ui/core';
 import { useLocation } from 'react-router-dom';
 //Components
 import CrossData from '../Components/CrossData';
 import Header from '../Components/Header';
-import StopWatch from '../Components/StopWatch';
+import Timer from '../Components/Timer';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -26,13 +26,17 @@ const CompetitionCross = () => {
     var endTime = 0;
     const isCompetition = location.state.competition;
     const sendToCompetition = location.state.sendTo;
-    
+
+    useEffect(() => {
+        const timer = setTimeout(() => { }, 1800);
+        return () => clearTimeout(timer);
+    }, []);
 
     // var callbackFunction = (childData) => {
     //     endTime = childData;
     //     localStorage.setItem("endTime", endTime);   
     // }
- 
+
 
 
     return (
@@ -40,9 +44,8 @@ const CompetitionCross = () => {
             <Header title={'תשבץ תחרות'} goBack={'/HomePage'} />
             <Container component="main" maxWidth="xs">
                 <div className={classes.paper}>
-                    <StopWatch/>
-                    {/* <StopWatch  parentCallback = {callbackFunction} /> */}
-                    <CrossData IsCompetition= {isCompetition} SendToCompetition={sendToCompetition} EndTime = {endTime} />
+                    <Timer></Timer>
+                    <CrossData IsCompetition={isCompetition} SendToCompetition={sendToCompetition} />
                 </div>
             </Container>
         </div>
